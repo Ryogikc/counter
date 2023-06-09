bd1a15848aaf0a2c2f0c76c6624426a821e9f23a
kcvarela <lic.karyna.campero@gmail.com>
2023-06-08 20:35:16 -0600

    theme type modified

---
+++ app/src/main/java/com/kcv/counter/ui/ItemCounterColumn.kt
@@ -0,0 +1,13 @@
+package com.kcv.counter.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.kcv.counter.data.local.Item
+
+@Composable
+class ItemCounterGrid(
+    itemList: List<Item>,
+  modifier: Modifier = Modifier,
+) {
+
+}
---
+++ app/src/main/java/com/kcv/counter/ui/itemCounter.kt
@@ -0,0 +1,4 @@
+package com.kcv.counter.ui
+
+class itemCounter {
+}
