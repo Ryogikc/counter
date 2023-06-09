9342ad549bd0ce0d913efbd4fee0453dd03ff3ca
kcvarela <lic.karyna.campero@gmail.com>
2023-06-09 13:22:21 -0600

    items composables created

--- app/src/main/java/com/kcv/counter/ui/ItemCounterColumn.kt
+++ app/src/main/java/com/kcv/counter/ui/ItemCounterColumn.kt
@@ -1,13 +1,37 @@
 package com.kcv.counter.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.kcv.counter.data.local.Item
 
 @Composable
-class ItemCounterGrid(
+fun ItemCounterColumn(
     itemList: List<Item>,
-  modifier: Modifier = Modifier,
-) {
-
+    onMinusClick: () -> Unit,
+    onPlusClick: () -> Unit,
+    onDeleteItemClick: (Item) -> Unit,
+    modifier: Modifier = Modifier,
+    ) {
+    Column(
+    modifier = modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LazyColumn {
+            items(itemList) { item ->
+                itemCounter(
+                    item = item,
+                    onMinusClick = onMinusClick,
+                    onPlusClick = onPlusClick,
+                    onDeleteItemClick = onDeleteItemClick
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/kcv/counter/ui/itemCounter.kt
+++ app/src/main/java/com/kcv/counter/ui/itemCounter.kt
@@ -1,4 +1,53 @@
 package com.kcv.counter.ui
 
-class itemCounter {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.R
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.kcv.counter.data.local.Item
+
+@Composable
+fun itemCounter(
+    item: Item,
+    onMinusClick: () -> Unit,
+    onPlusClick: () -> Unit,
+    onDeleteItemClick: (Item) -> Unit,
+) {
+  Column(modifier = Modifier
+      .height(180.dp)) {
+      Row {
+          Text(text = item.title,
+              textAlign = TextAlign.Center,
+              style = MaterialTheme.typography.bodyLarge,
+              modifier = Modifier.padding(vertical = 6.dp, horizontal = 6.dp))
+
+          Image(painter = painterResource(id = android.R.drawable.btn_minus),
+              contentDescription = null,
+              modifier = Modifier.padding(horizontal = 6.dp)
+                  .clickable { onMinusClick() })
+          Text(text = item.count.toString(),
+              textAlign = TextAlign.Center,
+              style = MaterialTheme.typography.bodyLarge,
+              modifier = Modifier.padding(vertical = 6.dp, horizontal = 6.dp))
+          Image(painter = painterResource(id = android.R.drawable.btn_plus),
+              contentDescription = null,
+              modifier = Modifier.padding(horizontal = 6.dp)
+              .clickable { onPlusClick() })
+          Image(painter = painterResource(id = android.R.drawable.ic_delete),
+              contentDescription = null,
+              modifier = Modifier.padding(horizontal = 6.dp)
+              .clickable { onDeleteItemClick(item)})
+      }
+  }
 }
