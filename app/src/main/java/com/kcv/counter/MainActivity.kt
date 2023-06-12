ff929dfa7eb427dd3132d9dc878d9981705ef583
kcvarela <lic.karyna.campero@gmail.com>
2023-06-11 18:07:11 -0600

    application configuration added

--- app/src/main/java/com/kcv/counter/MainActivity.kt
+++ app/src/main/java/com/kcv/counter/MainActivity.kt
@@ -12,16 +12,23 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.kcv.counter.ui.theme.CounterTheme
-import androidx.navigation.compose.rememberNavController
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.kcv.counter.ui.ItemCounterColumn
 import com.kcv.counter.ui.createCounterRow
 import com.kcv.counter.ui.sumRow
+import com.kcv.counter.ui.vm.ItemViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,25 +48,35 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun CounterApp() {
-    val navController = rememberNavController()
+private fun CounterApp(
+    itemViewModel: ItemViewModel = hiltViewModel(),
+) {
+   // val navController = rememberNavController()
+    val itemUiState by itemViewModel.uiState.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
         topBar = { TopBar() },
         content = { padding ->
+
             createCounterRow(
-                itemName =,
-                itemCount =,
-                titleToSearchChanged =,
-                onAddItemClick = { /*TODO*/ },
+                itemName = itemViewModel.itemName,
+                itemCount = itemViewModel.itemCounter.toString(),
+                itemNameChanged = {itemViewModel.updateItemName(it)},
+                itemCountChanged = {itemViewModel.updateCounterName(it.toInt())},
+                onAddItemClick = {
+                    coroutineScope.launch {
+                    itemViewModel.newItem(it.title, it.count)
+                } },
                 modifier = Modifier.padding(padding))
 
             ItemCounterColumn(
-                itemList =,
+                itemList = itemUiState.counterList,
                 onMinusClick = { /*TODO*/ },
                 onPlusClick = { /*TODO*/ },
-                onDeleteItemClick =
+                onDeleteItemClick = {}
             )
-            sumRow(itemCount =, onDeleteAllItems = { /*TODO*/ })
+            sumRow(itemCount = "", onDeleteAllItems = { /*TODO*/ })
         },
     )
 }
@@ -73,13 +90,10 @@ private fun TopBar(){
     )
 }
 
-
-
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CounterTheme {
-
+        CounterApp()
     }
 }
